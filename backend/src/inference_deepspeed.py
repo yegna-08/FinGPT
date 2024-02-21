042a96a8cc3ef4c1696bf38d509c42322963d7ad
yegna08 <yegna@northwestern.edu>
2024-02-21 09:08:02 +0530

    Testing deepspeed raw version

--- backend/src/inference_deepspeed.py
+++ backend/src/inference_deepspeed.py
@@ -1,7 +1,7 @@
 import os
 import torch
 import deepspeed
-from transformers import pipeline, LlamaTokenizer, LlamaForCausalLM
+from transformers import pipeline
 import google.protobuf
 
 def main():
@@ -12,32 +12,29 @@ def main():
     if torch.cuda.is_available():
         torch.cuda.set_device(local_rank)
 
-    # Finma model setup
-    finma_tokenizer = LlamaTokenizer.from_pretrained('ChanceFocus/finma-7b-trade')
-    finma_model = LlamaForCausalLM.from_pretrained('ChanceFocus/finma-7b-trade', device_map='auto')
+    # # Finma model setup
+    # finma_tokenizer = LlamaTokenizer.from_pretrained('ChanceFocus/finma-7b-trade')
+    # finma_model = LlamaForCausalLM.from_pretrained('ChanceFocus/finma-7b-trade', device_map='auto')
 
     # Initialize the pipeline with specific device
-    # generator = pipeline('text-generation', model='ChanceFocus/finma-7b-trade',
-    #                      device=local_rank)
+    generator = pipeline('text-generation', model='ChanceFocus/finma-7b-trade',
+                         device=local_rank)
 
     # Initialize DeepSpeed Inference
-    ds_engine = deepspeed.init_inference(finma_model,
+    generator.model = deepspeed.init_inference(generator.model,
                                                tensor_parallel={"tp_size": world_size},
                                                dtype=torch.float)
                                             #    replace_with_kernel_inject=True
-    
-    model = ds_engine.module
 
     # Efficient CUDA memory handling
     torch.cuda.empty_cache()  # Clear any cached memory to avoid out-of-memory
 
     with torch.no_grad():  # Ensures no gradients are computed to save memory
-        # string = generator("DeepSpeed is", do_sample=True, min_length=50)
-        output = model("Deepspeed is")
+        string = generator("DeepSpeed is", do_sample=True, min_length=50)
 
     # Ensure printing only occurs in the main process in a distributed setting
     if not torch.distributed.is_initialized() or torch.distributed.get_rank() == 0:
-        print(output)
+        print(string)
 
 if __name__ == "__main__":
     main()
